814051361f9510428b42512320c301acb6da58c5
Joao Almeida <joaofilipe90@gmail.com>
2022-03-01 17:15:09 +0000

    start

--- app/plumber.R
+++ app/plumber.R
@@ -1,10 +1,10 @@
 library(plumber)
 library(bnlearn)
 
-#load net
-
-model <- load("obs-hc.rda")
-
+#* @apiTitle Gapminder API
+#* @apiDescription API for exploring Gapminder dataset
+load("/Users/joaoalmeida/obs-hc.rda")
+model <- osa.hc.fit
 cors<-function(req,res){
   res$setHeader("Acess-Control-Allow-Origin","*")
   if (req$REQUEST_METHOD=="OPTIONS"){
@@ -16,15 +16,36 @@ cors<-function(req,res){
     plumber::forward()
   }
 }
+#* @post /eval_quality/<target>
+#* @param target:string
+#* @parser json
+#* @serializer json
 
-#* @post /predict
-function(req,res){
+function(req,res,target){
   data <-tryCatch(jsonlite::parse_json(req$postBody,simplifyVector=TRUE), error=function(e) NULL  )
+  #data <- req$postBody
   if (is.null(data)){
     res$status<-400
     list(error="No data")
   } 
-  data <-data.frame(data)
-  ret <- predict(model,data)
-  ret
+  data<-data.frame(data)
+  data <- as.data.frame(lapply(data,as.numeric))
+
+  predicted <- predict(model, node = target, data = data, method = "bayes-lw")
+  
+  real.val<-data[,target]
+  if ((real.val - predicted)>predicted*1.5){
+    ret=list(result="weak outlier",predicted=predicted,real=real.val)
+  }  else if ((real.val - predicted)>predicted*3){
+    ret=list(result="strong outlier",predicted=predicted,real=real.val)
+  }else{
+    ret=list(result="ok",predicted=predicted,real=real.val)
+    
+  }
+ret
+}
+#* @plumber
+function(pr) {
+  pr %>%
+    pr_set_api_spec("api-spec.yaml")
 }
